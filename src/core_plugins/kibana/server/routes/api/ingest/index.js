9470b86a95aa578abf2f8228e433c24ed62a5edf
SANGSOO KIM <lonycell@gmail.com>
2017-03-16 16:35:55 +0900

    thingspin: Upload CSV feature back ported from v5.0.0-alpha4 to v6.0.0-alpha1.

--- src/core_plugins/kibana/server/routes/api/ingest/index.js
+++ src/core_plugins/kibana/server/routes/api/ingest/index.js
@@ -1,5 +1,12 @@
 import { registerFieldCapabilities } from './register_field_capabilities';
 
+import { registerPost } from './register_post';
+import { registerDelete } from './register_delete';
+import { registerData } from './register_data';
+
 export default function (server) {
+  registerPost(server);
+  registerDelete(server);
+  registerData(server);
   registerFieldCapabilities(server);
 }
